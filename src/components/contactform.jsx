e577c6a88a515b5c5ef1197332765955dd867e24
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-08-15 19:27:36 +0530

    Added: EmailJS

--- src/components/contactform.jsx
+++ src/components/contactform.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/contactform.css";
 import twitter from "../images/icons/twittercontact.svg";
 import linkedin from "../images/icons/linkedincontact.svg";
 import phonecontact from "../images/icons/phonecontact.png";
 import emailcontact from "../images/icons/emailcontact.png";
+import emailjs from "@emailjs/browser";
 export default function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const serviceId = "service_tnhmq86";
+    const templateId = "template_rbvaso9";
+    const publicKey = "SbvegaP1b1gXHPkSL";
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      from_phone: phone,
+      message: message,
+    };
+    emailjs.send(serviceId, templateId, templateParams, publicKey).then(
+      (result) => {
+        setName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+
+        alert("Message Sent Successfully");
+      },
+      (error) => {
+        alert("Error Sending Message");
+      }
+    );
+  };
   return (
     <div className="contactmain">
       <div className="contactform">
@@ -47,24 +78,39 @@ export default function ContactForm() {
         </div>
         <div className="contactformright">
           <h4>Fill In Your Details</h4>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Enter Your Name"
               name="name"
               required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
             <div className="inputdivhalf">
-              <input type="email" placeholder="Enter Your Email" required />
+              <input
+                type="email"
+                placeholder="Enter Your Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
               <input
                 type="number"
                 placeholder="Enter Your Phone Number"
                 required
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
               />
             </div>
-            <textarea placeholder="Enter Your Message" required></textarea>
-            <button>Submit</button>
+            <textarea
+              placeholder="Enter Your Message"
+              required
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <button type="submit">Submit</button>
           </form>
         </div>
       </div>
