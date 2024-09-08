b186786b59f6fc4e10fda54683b3735a2c34ec4c
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-09-08 19:13:42 +0530

    Added: Stamp 4

--- src/pages/stamp4.jsx
+++ src/pages/stamp4.jsx
@@ -5,259 +5,173 @@ import Footer from "../components/footer";
 import immigrationImage from "../images/immigrationpages.jpeg";
 import { useNavigate } from "react-router-dom";
 import ContactForm from "../components/contactform";
-const SpouseCivilPartnerVisa = () => {
-  return (
-    <div className="criticalskillsbodyelement">
-      <h3>Spouse/Civil Partner Visa</h3>
-      <h4>Introduction</h4>
-      <p>
-        If you are married to or in a civil partnership with an Irish, UK, EEA,
-        or Swiss national, or a non-EEA national holding a work permit in
-        Ireland, you might be eligible to live in Ireland with your spouse or
-        partner. For those from visa-required countries wanting to stay for more
-        than three months, a long-stay visa or preclearance is necessary. If you
-        are from a non-visa-required country, no visa or preclearance is needed
-        unless your sponsor is a UK national.
-      </p>
-      <p>
-        The processing time for this visa typically ranges from four to six
-        months. It is advisable to wait for the visa decision before making any
-        travel arrangements.
-      </p>
-      <h4>How to Apply</h4>
-      <p>Ensure that:</p>
-      <ul>
-        <li>Your spouse or partner qualifies as a sponsor for this visa.</li>
-        <li>
-          Your sponsor meets the required minimum earnings threshold, which
-          varies based on their status.
-        </li>
-        <li>You are ordinarily resident outside Ireland.</li>
-        <li>You are of good character and standing.</li>
-        <li>You intend to live together permanently with your sponsor.</li>
-      </ul>
-      <p>
-        The application process is completed online. After filling out the
-        online form, print, sign, and date it, then submit it along with your
-        supporting documents. Biometrics may also be required.
-      </p>
-      <p>
-        <strong>Important:</strong> Providing false or misleading information
-        may result in visa refusal and could bar you from obtaining an Irish
-        visa for up to five years.
-      </p>
-      <h4>Eligibility & Work Permission</h4>
-      <p>
-        Sponsored by an Irish National: If your sponsor is an Irish national,
-        you can apply for this visa immediately and, if approved, receive a
-        Stamp 4, allowing you to live and work in Ireland without an employment
-        permit.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a Critical Skills Employment Permit:
-        You can apply immediately and will receive a Stamp 1G, permitting you to
-        live and work in Ireland without an employment permit.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a General Skills Employment Permit:
-        You may apply only after the sponsor has held the permit for one year.
-        If approved, you will receive a Stamp 3, which requires you to obtain a
-        separate work permit to work in Ireland.
-      </p>
-      <p>
-        Sponsored by a UK National: You can apply immediately and, if approved,
-        receive a Stamp 4, allowing you to live and work in Ireland without an
-        employment permit.
-      </p>
-      <p>
-        Sponsored by an EEA or Swiss National: Apply immediately and receive a
-        Stamp 4 if approved, allowing you to live and work in Ireland without an
-        employment permit.
-      </p>
-    </div>
-  );
-};
 
-const DeFactoPartner = () => {
+const Introduction = () => {
   return (
     <div className="criticalskillsbodyelement">
-      <h3>De Facto Partner Visa</h3>
-      <h4>Introduction</h4>
+      <h3>Introduction</h3>
       <p>
-        A de facto partner is someone with whom you have a committed
-        relationship similar to marriage or civil partnership, but without legal
-        status. To qualify, your relationship must:
+        Stamp 4 is immigration permission for foreign nationals to work, live,
+        study, and operate businesses in Ireland without an employment permit.
+        Stamp 4 stands out among all Irish immigration stamps as the ultimate
+        boon for immigrants that are non-EEA nationals. Typically, citizens from
+        a non-visa-required country (or spouse/civil partner to one) don’t need
+        to apply for a visa or employment permit to enter Ireland.
+      </p>
+      <p>
+        If you’re not a citizen of Ireland and want to stay in Ireland for more
+        than 90 days, ordinarily, you need to get immigration permission and an
+        employment permit. However, with Stamp 4, you are not restricted to
+        conditions and permits. You can do the following things:
       </p>
       <ul>
-        <li>Be genuine and ongoing.</li>
-        <li>Have lasted at least two years of cohabitation.</li>
-        <li>Not be a familial relationship.</li>
-      </ul>
-      <p>
-        If you are a de facto partner of an Irish, UK, EEA, or Swiss national,
-        or a non-EEA national with a work permit in Ireland, you may be allowed
-        to live in Ireland with your partner. Visa requirements are similar to
-        those for spouses and civil partners.
-      </p>
-      <h4>How to Apply</h4>
-      <p>Ensure that:</p>
-      <ul>
-        <li>Your partner qualifies as a sponsor.</li>
-        <li>The sponsor meets the minimum earnings requirement.</li>
-        <li>You are ordinarily resident outside Ireland.</li>
-        <li>You are of good character.</li>
-        <li>You have lived together for at least two years.</li>
-      </ul>
-      <p>
-        Submit your application online, print, sign, and date the form, and
-        provide all required documentation. Biometrics may be necessary.
-      </p>
-      <p>
-        <strong>Important:</strong> Providing false information can lead to visa
-        refusal and a potential five-year ban from obtaining an Irish visa.
-      </p>
-      <h4>Eligibility & Work Permission</h4>
-      <p>
-        Sponsored by an Irish National: Apply immediately and, if approved,
-        receive a Stamp 4 to live and work in Ireland.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a Critical Skills Employment Permit:
-        Apply immediately and receive a Stamp 1G, permitting you to live and
-        work in Ireland.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a General Skills Employment Permit:
-        Apply only after the sponsor has held the permit for one year. If
-        approved, you will get a Stamp 3, requiring a separate work permit.
-      </p>
-      <p>
-        Sponsored by a UK National: Apply immediately and receive a Stamp 4 if
-        approved, allowing you to live and work in Ireland.
-      </p>
-      <p>
-        Sponsored by an EEA or Swiss National: Apply immediately and receive a
-        Stamp 4, allowing you to live and work in Ireland.
-      </p>
-    </div>
-  );
-};
-
-const ElderlyParent = () => {
-  return (
-    <div className="criticalskillsbodyelement">
-      <h3>Elderly Parent Visa</h3>
-      <h4>Introduction</h4>
-      <p>
-        If you are an elderly relative of an Irish, UK, EEA, or Swiss national,
-        or a non-EEA national with a work permit in Ireland, you may be eligible
-        to live with your children in Ireland. If you are from a visa-required
-        country and wish to stay for more than three months, you must apply for
-        a long-stay visa. Non-visa-required country nationals do not need a visa
-        or preclearance unless the sponsor is a UK National.
-      </p>
-      <h4>How to Apply</h4>
-      <p>Ensure that:</p>
-      <ul>
-        <li>Your children qualify as sponsors.</li>
-        <li>The sponsor meets the minimum earnings requirement.</li>
-        <li>You are ordinarily resident outside Ireland.</li>
-        <li>You are of good character.</li>
+        <li>Work without an employment permit in any profession.</li>
+        <li>Start and run a business.</li>
         <li>
-          The sponsor has been legally resident in Ireland for at least three
-          years.
+          Access state funds and services (subject to Government departments or
+          agencies’ provisions).
         </li>
       </ul>
-      <p>
-        Submit your application online, print, sign, and date the form, and
-        provide necessary documentation. Biometrics may be required.
-      </p>
-      <p>
-        <strong>Important:</strong> Misleading information can result in visa
-        refusal and a potential five-year ban.
-      </p>
-      <h4>Eligibility & Work Permission</h4>
-      <p>
-        Sponsored by an Irish National: Apply immediately and, if approved,
-        receive a Stamp 0, allowing you to live in Ireland without being
-        involved in employment.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a Critical Skills Employment Permit:
-        Apply immediately and receive a Stamp 0 if approved, permitting you to
-        live in Ireland without employment.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a General Skills Employment Permit:
-        Apply only after the sponsor has held the permit for one year. If
-        approved, you will receive a Stamp 0.
-      </p>
-      <p>
-        Sponsored by a UK National: Apply immediately and receive a Stamp 0 if
-        approved, allowing you to live in Ireland.
-      </p>
-      <p>
-        Sponsored by an EEA or Swiss National: Apply immediately and receive a
-        Stamp 0 if approved, allowing you to live in Ireland.
-      </p>
     </div>
   );
 };
 
-const DependentChildren = () => {
+const WhoCanApply = () => {
   return (
     <div className="criticalskillsbodyelement">
-      <h3>Dependent Children Visa</h3>
-      <h4>Introduction</h4>
+      <h3>Who Can Apply for Stamp 4?</h3>
       <p>
-        If you are a dependent child of an Irish, UK, EEA, or Swiss national, or
-        a non-EEA national work permit holder in Ireland, you may be eligible to
-        live with your parent in Ireland. If you are from a visa-required
-        country and wish to stay for more than three months, you need a
-        long-stay visa. If you are from a non-visa-required country, no visa or
-        preclearance is needed unless your sponsor is a UK National.
+        You can take up employment and are not required to hold an Employment
+        Permit. You can work in a profession, subject to conditions of the
+        relevant professional or other bodies.
       </p>
-      <h4>How to Apply</h4>
-      <p>Ensure that:</p>
+      <p>
+        You can establish and operate a business. You may access state funds and
+        services as determined by Government departments or agencies.
+      </p>
+      <p>You may be given Stamp 4 after:</p>
       <ul>
-        <li>Your parent qualifies as a sponsor.</li>
-        <li>The sponsor meets the minimum earnings requirement.</li>
-        <li>You are ordinarily resident outside Ireland.</li>
-        <li>You are under 18 or under 23 if in full-time education.</li>
+        <li>Holding a valid Critical Skills Employment Permit for 2 years</li>
+        <li>Holding a valid employment permit for 5 years</li>
+        <li>Being a researcher with a valid Hosting Agreement for 2 years</li>
+      </ul>
+      <p>Other cases for Stamp 4 eligibility include:</p>
+      <ul>
+        <li>
+          Joining and residing with your Irish spouse, civil partner, or
+          de-facto partner
+        </li>
+        <li>Being a convention or programme refugee</li>
+        <li>
+          Joining a family member who is a recognised refugee or has subsidiary
+          protection
+        </li>
+        <li>Remaining with your Irish citizen child</li>
+        <li>Being part of the Investor and Entrepreneur Programme</li>
+        <li>For Long Term Residence</li>
+      </ul>
+    </div>
+  );
+};
+
+const EligibilityRequirements = () => {
+  return (
+    <div className="criticalskillsbodyelement">
+      <h3>Eligibility Requirements for Stamp 4</h3>
+      <p>Your eligibility requirements depend on your visa programme.</p>
+
+      <h4>For Start-up Entrepreneur Programme (STEP)</h4>
+      <p>The eligibility requirements for STEP are:</p>
+      <ul>
+        <li>You must be of good character.</li>
+        <li>
+          You must be free of criminal charges or convictions in any
+          jurisdiction.
+        </li>
+        <li>You must have €50,000 for the required funding.</li>
+        <li>You must have an innovative business proposal.</li>
+      </ul>
+
+      <h4>For Spouse, Civil Partner or De-facto Partners</h4>
+      <p>The eligibility requirements are:</p>
+      <ul>
+        <li>
+          You must not be younger than 18 years of age at the time of your
+          application for family reunification.
+        </li>
+        <li>
+          Your marriage must be monogamous, consensual, legalised, and
+          recognised by Irish law.
+        </li>
+        <li>
+          Your partnerships must be similar to marriage and cohabiting for two
+          years before the family application.
+        </li>
+        <li>
+          Demonstrate commitment to be together permanently as couples, civil
+          partners or de facto partners.
+        </li>
+      </ul>
+
+      <h4>For Long Term Residency</h4>
+      <p>
+        If you have been offered an Irish Immigration permit, your eligibility
+        requirements are as follows:
+      </p>
+      <ul>
+        <li>
+          You must have completed at least 5-year legal residence in Ireland.
+        </li>
+        <li>
+          You must have qualifying stamps or valid Irish residence permit (IRP)
+          cards as proof of 5-year legal residency.
+        </li>
+        <li>
+          You must have been an employment permit or critical skills employment
+          permit holder.
+        </li>
+        <li>You must have been of good character.</li>
+        <li>
+          You must not have caused an unnecessary burden on the government.
+        </li>
+        <li>You must not have an issue or record with the Irish police.</li>
+        <li>
+          You must have been compliant with all your previous immigration
+          permissions.
+        </li>
+        <li>You must be a legal resident at the application.</li>
+        <li>
+          You must be in employment before, during, and after your application.
+          Self-employment is not allowed.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const ApplicationProcess = () => {
+  return (
+    <div className="criticalskillsbodyelement">
+      <h3>Application Process</h3>
+      <p>
+        The process for applying for Stamp 4 varies based on the specific visa
+        or residency permission you are applying for. Generally, you will need
+        to follow these steps:
+      </p>
+      <ul>
+        <li>
+          Submit an online application through the appropriate immigration
+          portal.
+        </li>
+        <li>
+          Print, sign, and submit the application along with the necessary
+          documents.
+        </li>
+        <li>Provide biometric data if required.</li>
       </ul>
       <p>
-        Submit your application online, print, sign, and date the form, and
-        provide required documentation. Biometrics may be required.
-      </p>
-      <p>
-        <strong>Important:</strong> Misleading information can result in visa
-        refusal and a potential five-year ban.
-      </p>
-      <h4>Eligibility & Work Permission</h4>
-      <p>
-        Sponsored by an Irish National: Apply immediately and, if approved,
-        receive a Stamp 3, allowing you to live in Ireland, but you will need a
-        separate work permit to work.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a Critical Skills Employment Permit:
-        Apply immediately and receive a Stamp 3 if approved, permitting you to
-        live in Ireland, though a separate work permit is needed to work.
-      </p>
-      <p>
-        Sponsored by a Non-EEA national on a General Skills Employment Permit:
-        Apply only after the sponsor has held the permit for one year. If
-        approved, you will receive a Stamp 3, requiring a separate work permit.
-      </p>
-      <p>
-        Sponsored by a UK National: Apply immediately and receive a Stamp 3 if
-        approved, allowing you to live in Ireland, though a separate work permit
-        is required.
-      </p>
-      <p>
-        Sponsored by an EEA or Swiss National: Apply immediately and receive a
-        Stamp 3 if approved, permitting you to live in Ireland with a separate
-        work permit required.
+        <strong>Important:</strong> Submitting false or misleading information
+        may result in visa refusal and a ban on reapplying for up to five years.
       </p>
     </div>
   );
@@ -265,6 +179,7 @@ const DependentChildren = () => {
 export default function Stamp4() {
   const [selectedMode, setSelectedMode] = useState(1);
   const navigate = useNavigate();
+
   return (
     <div className="criticalskillsmain">
       <Navbar />
@@ -282,12 +197,9 @@ export default function Stamp4() {
           </p>
           <h1>Stamp 4</h1>
           <p>
-            If you are married to, in a civil partnership with, or a dependent
-            relative of an Irish, UK, EEA, or Swiss national, or a non-EEA
-            national work permit holder in Ireland, you may be eligible to live
-            in Ireland with your family member. Here’s what you need to know
-            about the application process and requirements for various family
-            visas.
+            Stamp 4 is immigration permission that allows foreign nationals to
+            work, live, study, and operate businesses in Ireland without an
+            employment permit.
           </p>
           <button
             className="contactusbutton"
@@ -300,6 +212,7 @@ export default function Stamp4() {
           <img src={immigrationImage} alt="Immigration" />
         </div>
       </div>
+
       <div className="criticalskillbody">
         <div className="workpagecomponentselector">
           <ul>
@@ -307,35 +220,37 @@ export default function Stamp4() {
               onClick={() => setSelectedMode(1)}
               className={selectedMode === 1 ? "selectedelement" : ""}
             >
-              Spouse/ Civil Partner Visa
+              Introduction
             </li>
             <li
               onClick={() => setSelectedMode(2)}
               className={selectedMode === 2 ? "selectedelement" : ""}
             >
-              De Factor Partner
+              Who Can Apply
             </li>
             <li
               onClick={() => setSelectedMode(3)}
               className={selectedMode === 3 ? "selectedelement" : ""}
             >
-              Elderly Parent
+              Eligibility Requirements
             </li>
             <li
               onClick={() => setSelectedMode(4)}
               className={selectedMode === 4 ? "selectedelement" : ""}
             >
-              Dependant Childern
+              Application Process
             </li>
           </ul>
         </div>
       </div>
+
       <div className="criticalpagecontent">
-        {selectedMode === 1 && <SpouseCivilPartnerVisa />}
-        {selectedMode === 2 && <DeFactoPartner />}
-        {selectedMode === 3 && <ElderlyParent />}
-        {selectedMode === 4 && <DependentChildren />}
+        {selectedMode === 1 && <Introduction />}
+        {selectedMode === 2 && <WhoCanApply />}
+        {selectedMode === 3 && <EligibilityRequirements />}
+        {selectedMode === 4 && <ApplicationProcess />}
       </div>
+
       <ContactForm />
       <Footer />
     </div>
